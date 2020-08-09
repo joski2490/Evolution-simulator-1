4b14ed9e624487cd80295096add4359564ab02a6
Luca De Rosa <67323776+LJDR17@users.noreply.github.com>
2020-08-09 13:15:28 +0100

    Add files via upload

---
+++ JS charts.js
@@ -0,0 +1,44 @@
+new Chart(document.getElementById("line-chart"), {
+    type: 'line',
+    data: {
+      labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+      datasets: [{ 
+          data: [86,114,106,106,107,111,133,221,783,2478],
+          label: "test",
+          borderColor: "#3e95cd",
+          fill: false
+        }, 
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'test'
+      },
+      responsive: false
+    }
+});
+
+new Chart(document.getElementById("line-chart2"), {
+    type: 'line',
+    data: {
+      labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+      datasets: [{ 
+          data: [86,114,106,106,107,111,133,221,783,2478],
+          label: "test",
+          borderColor: "#3e95cd",
+          fill: false
+        }, 
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'test'
+      },
+      responsive: false
+    }
+});
+
+
+  
