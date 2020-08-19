67216596b4535db2ee706b27633ea7cc45c56954
Luca De Rosa <67323776+LJDR17@users.noreply.github.com>
2020-08-19 10:21:35 +0100

    Update JS project.js

--- JS project.js
+++ JS project.js
@@ -1,43 +1,112 @@
-var players = [];
+var creatures = [];
 var food = [];
 var iteration = 0;
 var highestScore = 0;
 
 function setup() {
-    var canvas = createCanvas(900, 800);
-    canvas.id('canvas');
+  var canvas = createCanvas(900, 800);
+  
+  canvas.id('canvas');
+  canvas.position(10,100);
+  initNeat();
+  // Triggers food spawning and limits amount of food
+  for(var i = 0; i < foodAmount; i++){
+  // As food is an object, 'new' calls in a new instance of food
+  new Food();
+  }
 
-    // Triggers food spawning and limits amount of food
-    for(var i = 0; i < 10; i++){
-    // As food is an object, 'new' calls in a new instance of food
-    new Food();
-    }
+  for(var i = 0; i < 100; i++) neat.mutate();
+
+  startEvaluation();
 }
-
-
-function Food() {
-  // Creates random coordinates for food
-  this.x = Math.floor(Math.random() * 900);
-  this.y = Math.floor(Math.random() * 800);
-  // 'Push' adds 1 food particle to the array
-  food.push(this);
-}
-
-//Food is an object - the show function defines what the food looks like
-Food.prototype.show = function() {
-  fill('rgb(0,255,0)');
-  noStroke();
-  ellipse(this.x, this.y, 10); 
-}
+//Do update function next/creature functions. Ideally make creatures move, without nn for now.
+//All in Draw function
 
 /* 'Draw()' is part of P5.js and continually executes the lines of code 
 within it after the 'setup()' function has been triggered*/
+
 function draw() {
+  //makes sure the creature actually appears to move and doesnt leave a trail of where it was before
+  clear();
+
+  // Check if evaluation is done
+  if(iteration == iterations){
+    endEvaluation();
+    iteration = 0;
+  }
   for(var i = food.length - 1; i >= 0; i--){
     food[i].show();
   }
+  for(var i = creatures.length - 1; i >= 0; i--){
+    //refresh updates statistics
+    creatures[i].refresh();
+    //show then reflects these changes onto the canvas
+    creatures[i].show();
+  }
+
+  iteration++ /*adding the iteration counter here is useful because
+  the code here is continually looped. This means I don't have to try
+  and implement timer functions which are designed to execute code after a delay*/
 }
 
+function Creature(genome) {
+  this.x = Math.floor(Math.random() * 900);
+  this.y = Math.floor(Math.random() * 800);
+  this.velocityX = 0;
+  this.velocityY = 0;
+  
+  /*score is under gene because the score needs to be attached to the specific weights of the network,
+  there is an error otherwise*/
+  this.genes = genome;
+  this.score = 0;
+
+  creatures.push(this);
+}
+
+Creature.prototype.refresh = function() {
+
+  //angle needs to be connected to nn, it is fixed for test
+  var angle = 50;
+  var speed = 0.5;
+  
+  //https://stackoverflow.com/questions/22421054/determine-movement-vectors-direction-from-velocity
+  this.velocityX = speed * Math.cos(angle);
+  this.velocityY = speed * Math.sin(angle);
+  
+  this.x += this.velocityX;
+  this.y += this.velocityY;
+}
+
+//Restart from new position
+Creature.prototype.restart = function() {
+  this.x = Math.floor(Math.random() * 900);
+  this.y = Math.floor(Math.random() * 800);
+  this.velocityX = 0;
+  this.velocityX = 0;
+}
+
+Creature.prototype.show = function() {
+  fill('rgb(0,0,255)');
+  noStroke();
+  ellipse(this.x, this.y, 15); 
+}
+
+Creature.prototype.eat = function(food) {
+  var distance = distance(this.x, this.y, food.x, food.y);
+  //distance so small they're pretty much overlapping - collision detection
+  if (distance < 10) {
+    //score increases here
+    food.restart();
+  }
+}
+
+//distance between food and creature
+function distance(x1, x2, y1, y2) {
+  var distanceX = x2-x1;
+  var distanceY = y2-y1;
+  return distanceX * distanceX + distanceY * distanceY;
+  //https://stackoverflow.com/questions/20916953/get-distance-between-two-points-in-canvas
+}
 
 
 
