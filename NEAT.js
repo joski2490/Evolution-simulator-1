b682c8f4601f2f8c1f6d8750a9372be20211ccfe
Luca De Rosa <67323776+LJDR17@users.noreply.github.com>
2020-08-28 16:01:47 +0100

    Update NEAT.js

--- NEAT.js
+++ NEAT.js
@@ -1,15 +1,16 @@
 var detectionRadius = 150;
-var foodDetection  = 3;
+var foodDetection  = 4;
 
-var foodAmount = 150;
+var foodAmount = 50;
 
 var SPEED = 3;
 
-var creatureAmount = 20;
+var creatureAmount = 10;
 var iterations = 600;
-var startHiddenSize = 5;
-var mutationRate1 = 0.3;
-var elitismPercentage = 0.1;
+var startHiddenSize = 0;
+var mutationRate1 = 0.5;
+var mutationAmount1 = 1;
+var elitismPercentage = 0.3;
 
 var neat;
 
@@ -22,13 +23,14 @@ neataptic.Config.warnings = false;
 // Construct the genetic algorithm
 function initNeat(){
   //foodDetection is *2 because the angle and distance of food needs to be inputted
-  neat = new neataptic.Neat(foodDetection * 2, 2, null,
+  neat = new neataptic.Neat(2 + foodDetection * 2, 2, null,
     {
       mutation: neataptic.Methods.Mutation.ALL,
       popsize: creatureAmount,
       mutationRate: mutationRate1,
+      mutationAmount: mutationAmount1,
       elitism: Math.round(elitismPercentage * creatureAmount),
-      network: new neataptic.Architect.Random(foodDetection * 2, startHiddenSize, 2)
+      network: new neataptic.Architect.Random(2 + foodDetection * 2, startHiddenSize, 2)
     }
   );
 }
@@ -41,18 +43,18 @@ function startEvaluation(){
     for(var genome in neat.population){ 
       genome = neat.population[genome];
       new Creature(genome);
+      console.log(genome);
     }
 }
 
 //End the evaluation of the current generation
 function endEvaluation(){
-    console.log('Generation:', neat.generation, '- average score:', neat.getAverage());
-    
     //for graph
     generation = neat.generation;
     score = neat.getAverage();
-    addData(chart, generation, score)
-    addData(chart2, generation, highestScore)
+    addData(chart, generation, score);
+    addData(chart2, generation, highestScore);
+
     neat.sort();
     var newPopulation = [];
   
